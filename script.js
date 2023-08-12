c403aaf804748ba3102de8fe83ad5a8654cc5b01
Farhod <pecom@pecom.ru>
2023-08-12 16:57:45 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const a = confirm( "Are you here?");
+
+// console.log("a");
+
+
+const answer = [];
+
+answer [0] = prompt('What is your name?', '');
+answer [1] = prompt('How old are you', '');
+answer [2] = prompt('What is your surname', '');
+
+// console.log(answer);
+
+document.write(answer);
